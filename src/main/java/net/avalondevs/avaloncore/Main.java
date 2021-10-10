d2a19472a490ddc557c86e80941882b878a2b236
Nowilltolife <55301990+Nowilltolife@users.noreply.github.com>
2021-10-11 01:45:30 +0200

    Minor fixes

--- src/main/java/net/avalondevs/avaloncore/Main.java
+++ src/main/java/net/avalondevs/avaloncore/Main.java
@@ -6,10 +6,16 @@ import net.avalondevs.avaloncore.Commands.Staff.VanishCommand;
 import net.avalondevs.avaloncore.Commands.Tags.TagsCommand;
 import lombok.Getter;
 import lombok.Setter;
+import net.avalondevs.avaloncore.Commands.voucher.VoucherCommand;
+import net.avalondevs.avaloncore.Listeners.PlayerListeners;
 import net.avalondevs.avaloncore.MySQL.MySQL;
 import net.avalondevs.avaloncore.MySQL.PlayerData;
 import net.avalondevs.avaloncore.MySQL.SQLGetter;
 import net.avalondevs.avaloncore.MySQL.StaffSQL;
+import net.avalondevs.avaloncore.Utils.LuckPermsAdapter;
+import net.avalondevs.avaloncore.Utils.command.CommandFramework;
+import net.avalondevs.avaloncore.data.Voucher;
+import net.luckperms.api.LuckPermsProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -33,6 +39,8 @@ public final class Main extends JavaPlugin {
 
         instance = this;
 
+        LuckPermsAdapter.init();
+
         saveDefaultConfig();
 
         // Init MySQL Database
@@ -52,6 +60,12 @@ public final class Main extends JavaPlugin {
             Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "MYSQL DATABASE CONNECTED SUCCESSFULLY");
             loadCommands();
         }
+
+        loadModuleCommands();
+
+        Voucher.cache();
+
+        Bukkit.getPluginManager().registerEvents(new PlayerListeners(), this);
     }
 
     @Override
@@ -60,10 +74,21 @@ public final class Main extends JavaPlugin {
     }
 
 
+    /**
+     * This method will load the commands that don't depend on databasing to be enabled
+     */
+    void loadModuleCommands() {
+
+        CommandFramework framework = new CommandFramework(this); // initialize a new framework
+
+        framework.registerCommands(new VoucherCommand()); // load VoucherCommand into the framework
+        framework.registerCommands(new GamemodeCommand()); // load the GamemodeCommand
+
+    }
+
     void loadCommands() {
         new TagsCommand();
         new VanishCommand();
-        new GamemodeCommand();
         new FreezeeCommand();
     }
 
