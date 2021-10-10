2394eb0fc8abf7c6970a8537d848a0ebdda90b28
Nowilltolife <55301990+Nowilltolife@users.noreply.github.com>
2021-10-11 01:41:53 +0200

    Updated GamemodeCommand to new command systemnclude

--- src/main/java/net/avalondevs/avaloncore/Commands/Staff/GamemodeCommand.java
+++ src/main/java/net/avalondevs/avaloncore/Commands/Staff/GamemodeCommand.java
@@ -1,51 +1,41 @@
 package net.avalondevs.avaloncore.Commands.Staff;
 
 import net.avalondevs.avaloncore.Main;
+import net.avalondevs.avaloncore.Utils.StrUtil;
+import net.avalondevs.avaloncore.Utils.Utils;
+import net.avalondevs.avaloncore.Utils.command.Command;
+import net.avalondevs.avaloncore.Utils.command.CommandAdapter;
 import org.bukkit.GameMode;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class GamemodeCommand implements CommandExecutor {
+public class GamemodeCommand {
 
-    public GamemodeCommand() {
-        Main.getPlugin(Main.class).getCommand("gamemode").setExecutor(this);
+    public void commonStub(CommandAdapter adapter, GameMode mode) {
+
+        if(adapter.getSender().hasPermission("core.gamemode." + mode.name().toLowerCase())) {
+            adapter.getPlayer().setGameMode(GameMode.CREATIVE);
+            adapter.sendMessage(Utils.PREFIX + " &7Set your gamemode to &e&n" + StrUtil.extractNameFromEnum(mode));
+        } else {
+            adapter.sendNoPermission();
+        }
+
     }
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if(!(sender instanceof Player)) {
-            sender.sendMessage("Player only command!");
-        }
 
-        Player player = (Player) sender;
-        switch (args[0]) {
-            case "c":
-                if(player.hasPermission("core.gamemode.creative")) {
-                    player.setGameMode(GameMode.CREATIVE);
-                    player.sendMessage("&3&lAVA&B&LLON &7set your gamemode to &e&n" + args[0]);
-                } else {
-                    player.sendMessage("&3&lAVA&B&LLON &cYou do not have access to this command!");
-                }
-                break;
-            case "s":
-                if(player.hasPermission("core.gamemode.survival")) {
-                    player.setGameMode(GameMode.SURVIVAL);
-                    player.sendMessage("&3&lAVA&B&LLON &7set your gamemode to &e&n" + args[0]);
-                } else {
-                    player.sendMessage("&3&lAVA&B&LLON &cYou do not have access to this command!");
-                }
-                break;
-            case "sp":
-                if(player.hasPermission("core.gamemode.spectator")) {
-                    player.setGameMode(GameMode.SPECTATOR);
-                    player.sendMessage("&3&lAVA&B&LLON &7set your gamemode to &e&n" + args[0]);
-                } else {
-                    player.sendMessage("&3&lAVA&B&LLON &cYou do not have access to this command!");
-                }
-                break;
-        }
-        return true;
+    @Command(name = "gamemode", aliases = {"gm"}, permission = "core.gamemode")
+    public void onCommand(@NotNull CommandAdapter adapter) {
+
+        Player player = adapter.getPlayer();
+        adapter.requireArg(0, (mode) -> {
+
+            switch (mode) {
+                case "c" -> commonStub(adapter, GameMode.CREATIVE);
+                case "s" -> commonStub(adapter, GameMode.SURVIVAL);
+                case "sp" -> commonStub(adapter, GameMode.SPECTATOR);
+            }
+
+        });
     }
 }
